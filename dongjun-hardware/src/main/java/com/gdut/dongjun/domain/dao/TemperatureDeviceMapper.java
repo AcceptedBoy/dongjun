9f4c7bbc1ceb23780544641acc4f4e374ddbf627
bcdzc <394894672@qq.com>
2017-04-14 16:56:11 +0800

    删除多余代码

--- dongjun-hardware/src/main/java/com/gdut/dongjun/domain/dao/TemperatureDeviceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.gdut.dongjun.domain.dao;
-
-import com.gdut.dongjun.domain.dao.base.SinglePrimaryKeyBaseMapper;
-import com.gdut.dongjun.domain.po.TemperatureDevice;
-
-public interface TemperatureDeviceMapper 
-		extends SinglePrimaryKeyBaseMapper<TemperatureDevice> {
-
-}
