0e45e58b2d7298f819bdb2c97f50f1e0c5f1d3be
bcdzc <394894672@qq.com>
2017-05-03 15:26:09 +0800

    feat: 平台版电能表设备接口

--- dongjun-platform/src/main/java/com/gdut/dongjun/domain/dao/impl/TemperatureSensorDAOImpl.java
+++ dongjun-platform/src/main/java/com/gdut/dongjun/domain/dao/impl/TemperatureSensorDAOImpl.java
@@ -1,5 +1,7 @@
 package com.gdut.dongjun.domain.dao.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.gdut.dongjun.domain.dao.TemperatureSensorMapper;
@@ -8,5 +10,10 @@ import com.gdut.dongjun.domain.po.TemperatureSensor;
 
 @Repository
 public class TemperatureSensorDAOImpl extends SinglePrimaryKeyBaseDAOImpl<TemperatureSensor> implements TemperatureSensorMapper {
+
+	@Override
+	public List<TemperatureSensor> selectAllType(String id) {
+		return template.selectList(getNamespace("selectAllType"), id);
+	}
 
 }
