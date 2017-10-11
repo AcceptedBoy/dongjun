6b68d37f76eef04494dc3c7f955116ff526a5b03
bcdzc <394894672@qq.com>
2017-10-11 22:32:27 +0800

    feat: 语音提示按顺序播放

--- dongjun-website/src/main/java/com/gdut/dongjun/Application.java
+++ dongjun-website/src/main/java/com/gdut/dongjun/Application.java
@@ -60,9 +60,9 @@ public class Application extends SpringBootServletInitializer {
 	public DataSource dataSource() {
 
 		ComboPooledDataSource ds = new ComboPooledDataSource();
-		ds.setJdbcUrl("jdbc:mysql://115.28.7.40:3306/elecon1?useUnicode=true&amp;charaterEncoding=utf-8&zeroDateTimeBehavior=convertToNull");
+		ds.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/elecon?useUnicode=true&amp;charaterEncoding=utf-8&zeroDateTimeBehavior=convertToNull");
 		ds.setUser("root");
-		ds.setPassword("topview+624");//elecon
+		ds.setPassword("759486");//elecon
 //		ds.setJdbcUrl("jdbc:mysql://120.24.12.171:3306/elecon?useUnicode=true&amp;charaterEncoding=utf-8&zeroDateTimeBehavior=convertToNull");
 //		ds.setUser("topview");
 //		ds.setPassword("topview+624");//elecon
